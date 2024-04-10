92d2324ed193a8c71390e4b128805fa9f3df86f6
Luis carlos <luisxhl7@gmail.com>
2024-04-10 17:07:01 -0500

    adding setting of express and DB

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
+require('dotenv').config();
+
+// Crear el servidor de express
+const app = express();
+
+// Conexión a base de datos
+dbConnection();
+
+// Cors
+app.use(cors());
+
+// Lectura y parseo del body
+app.use( express.json());
+
+// Directorio publico
+app.use( express.static('public') );
+
+// Rutas
+// app.use('/api/auth', require('./routes/auth'));
+
+// Escuchar peticiones
+app.listen( process.env.PORT, () => {
+    console.log(`servidor corriendo en puerto ${process.env.PORT}`);
+})
